f0a3a9a69c2a2e96f895a700983d5d8e02929ff5
AnnaVitkina <anna_vitkina@trimble.com>
2025-12-04 19:33:30 +0200

    Update part7_optional_order_lc_etof_mapping.py

--- part7_optional_order_lc_etof_mapping.py
+++ part7_optional_order_lc_etof_mapping.py
@@ -271,7 +271,7 @@ def process_order_lc_etof_mapping(lc_input_path, etof_path, order_files_path=Non
     lc_dataframe_final, lc_column_names = map_etof_to_lc(etof_dataframe, lc_dataframe)
 
     if 'DELIVERY_NUMBER'in lc_dataframe_final.columns:
-        lc_dataframe_final = lc_dataframe_final.rename(columns=['DELIVERY_NUMBER': 'Delivery Number']
+        lc_dataframe_final = lc_dataframe_final.rename(columns={'DELIVERY_NUMBER': 'Delivery Number'})
         lc_column_names = lc_dataframe_final.columns.tolist()
     
     save_dataframe_to_excel(lc_dataframe_final, output_filename)
@@ -292,6 +292,7 @@ def process_order_lc_etof_mapping(lc_input_path, etof_path, order_files_path=Non
 #        etof_path, 
 #        order_files_path=order_files_path
 #    )
+
 
 
 
