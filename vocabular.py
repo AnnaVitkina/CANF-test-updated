97518c769823cf4ef10f49a1703a2f278c148265
AnnaVitkina <anna_vitkina@trimble.com>
2025-12-04 19:40:23 +0200

    Update vocabular.py

--- vocabular.py
+++ vocabular.py
@@ -776,11 +776,11 @@ def map_and_rename_columns(
         
         output_df = source_df.copy()
 
-        if 'DELIVERY_NUMBER'in output_df.columns:
-            output_df = output_df.rename(columns={'DELIVERY_NUMBER': 'Delivery Number'})
+        #if 'DELIVERY_NUMBER'in output_df.columns:
+            #output_df = output_df.rename(columns={'DELIVERY_NUMBER': 'Delivery Number'})
 
-        if 'DELIVERY NUMBER(s)'in output_df.columns:
-            output_df = output_df.rename(columns={'DELIVERY NUMBER(s)': 'Delivery Number'})
+        #if 'DELIVERY NUMBER(s)'in output_df.columns:
+            #output_df = output_df.rename(columns={'DELIVERY NUMBER(s)': 'Delivery Number'})
                                          
         rename_dict = {}
         columns_to_keep = []
@@ -1228,6 +1228,7 @@ def map_and_rename_columns(
  #   except Exception:
   #      pass
 #
+
 
 
 
